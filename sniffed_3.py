416833b18033f4e4c9a43f50d99edde71fb6bcd8
seigen96 <31039828+seigen96@users.noreply.github.com>
2018-10-11 17:07:39 +0800

    Add files via upload

---
+++ sniffed_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=false, prn=process_sniffed_packet)
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+sniff("eth0")
